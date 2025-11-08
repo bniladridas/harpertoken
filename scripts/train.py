9125e200a258698dbf07335af0ef4d966ca28172
Niladri Das <bniladridas@users.noreply.github.com>
2025-11-08 13:17:28 +0530

    fix: relative import and hf token

--- scripts/train.py
+++ scripts/train.py
@@ -7,7 +7,7 @@ from transformers import (
     Trainer,
     DefaultDataCollator,
 )
-from data_prep import load_and_preprocess_data
+from .data_prep import load_and_preprocess_data
 
 # Check for MPS availability
 device = (
